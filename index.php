93d38fb0b551c03861c79f5ec34e9a5e23963db3
ZufarMaliki <zufarmaliki10@gmail.com>
2026-05-21 10:58:27 +0700

    Menambahkan index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote Lagi</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+    <ul>
+        <li>List 1</li>
+        <li>List 2</li>
+        <li>List 3</li>
+    </ul>
+</body>
+</html>
